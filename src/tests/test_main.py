06319f0e28a228c810953ab1f8f7c8f5c3d86cad
John Lawson <john.edward.lawson@gmail.com>
2019-11-23 14:18:46 +0000

    added flake8 json tests

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -74,9 +74,15 @@ def test_get_weather(requests_mock):
     assert weather.wind_speed == 5.1
 
 
-# def test_get_current_weather_api():
-#     response = main.get_current_weather()
-#     assert response["temp"] == 
+def test_get_current_weather_api():
+    response = main.get_current_weather()
+    assert response["temp"] == 7.71
+    assert response["pressure"] == 995
+    assert response["humidity"] == 93
+    assert response["temp_max"] == 9.44
+    assert response["temp_min"] == 6.67
+    assert response["wind_angle"] == 100
+    assert response["wind_speed"] == 5.1
 
 
 def test_get_forcast(requests_mock):
@@ -93,3 +99,15 @@ def test_get_forcast(requests_mock):
     assert forcast[0].wind_speed == 9.11
     assert forcast[0].wind_angle == 124
 
+
+def test_get_current_weather_api():
+    response = main.get_forcast_weather()
+    assert len(response) == 40
+    assert response[0]["temp"] == 8.32
+    assert response[0]["pressure"] == 994
+    assert response[0]["humidity"] == 80
+    assert response[0]["temp_max"] == 9.32
+    assert response[0]["temp_min"] == 8.32
+    assert response[0]["datetime"] == '2019-11-23 00:00'
+    assert response[0]["wind_speed"] == 9.11
+    assert response[0]["wind_angle"] == 124
