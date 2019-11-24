9302b31b660147485f3cc5684958c87461765a91
John Lawson <john.edward.lawson@gmail.com>
2019-11-24 19:55:51 +0000

    restructured

--- web/static/main.js
+++ web/static/main.js
@@ -9,14 +9,15 @@ CURRENT_ENDPOINT = API_BASE + "/current_weather/";
 
 async function main(){
 
+    get_current_weather();
+}
+
+async function get_current_weather(){
     const data = await getApi(CURRENT_ENDPOINT);
-    document.getElementById("temp").innerHTML = data.temp + "°C";
-    document.getElementById("pressure").innerHTML = data.pressure + " hPa";
-    document.getElementById("humidity").innerHTML = data.humidity + "%";
-    document.getElementById("wind_speed").innerHTML = data.wind_speed + "km/h";
-    document.getElementById("wind_dir").innerHTML = data.wind_direction;
-    document.getElementById("temp_min").innerHTML = data.temp_min + "°C";
-    document.getElementById("temp_max").innerHTML = data.temp_max + "°C";
+    for(const [key, value] of Object.entries(data)) {
+        console.log(key, value);
+        document.getElementById("current_" + key).innerHTML = value;
+    }
 }
 
 async function getApi(url){
