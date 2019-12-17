944d313edf0a23187d14d3e6ee6ed6b573c5fbdc
John Lawson <john.edward.lawson@gmail.com>
2019-12-17 14:09:55 +0000

    improved run script for pi

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd ~/Documents/weather-station
+# cd ~/Documents/weather-stations
 source env/bin/activate
 # pip install -r requirements.txt
-cat exports.txt | while read line; do export $line; done
+source ./exports.sh 
 
 DISPLAY=:0 chromium-browser -start-maximized http://127.0.0.1:5000/ &
 
