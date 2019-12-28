fcaac3e2f95a5b45dd8cc2e57bf63dc466668113
John Lawson <john.edward.lawson@gmail.com>
2019-12-28 16:48:33 +0000

    add no cache headers

--- main.py
+++ main.py
@@ -35,5 +35,18 @@ def get_indoors_weather():
     return get_indoors()
 
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+
 if __name__ == "__main__":
     app.run(debug=True)
